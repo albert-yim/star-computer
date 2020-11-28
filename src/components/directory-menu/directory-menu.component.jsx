df9e6e043dd6f673f21c7fcba9e67e31395064b4
chanyang719 <chanyang0719@gmail.com>
2020-11-28 12:54:27 -0800

    template imge add

--- src/components/directory-menu/directory-menu.component.jsx
+++ src/components/directory-menu/directory-menu.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import MenuItem from '../menu-item/menu-item.component';
 import './directory-menu.styles.scss'
-
+import motherImg from '../../img/motherboard.png'
 class Directory extends React.Component{
     
     constructor(){
@@ -11,35 +11,41 @@ class Directory extends React.Component{
         this.state = {
             sections: [{
                 title : 'MotherBoard',
+                imgUrl : 'https://i.ibb.co/cvpntL1/hats.png',
                 id : 1
             },
             {
                 title : 'RAM',
+                imgUrl : 'https://i.ibb.co/px2tCc3/jackets.png',
                 id : 2
             }, 
             {
                 title : 'Power',
+                imgUrl : 'https://i.ibb.co/0jqHpnp/sneakers.png',
                 id : 3
             },
             {
                 title : 'CPU',
+                imgUrl : 'https://i.ibb.co/GCCdy8t/womens.png',
                 id : 4,
                 size : 'large'
             },
             {
                 title : 'GPU',
+                imgUrl : 'https://i.ibb.co/R70vBrQ/men.png',
                 id : 5,
                 size : 'large'
             }]
         }
     }
-    
+
+
     render() {
         return (
             <div className = 'directory-menu'>
                 {
-                    this.state.sections.map(({title,id,size}) => (
-                        <MenuItem key = {id} title = {title} size = {size}/>
+                    this.state.sections.map(({title,imgUrl,id,size}) => (
+                        <MenuItem key = {id} title = {title} imgUrl = {imgUrl} size = {size}/>
                     ))
                 }
             </div>
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -2,8 +2,13 @@ import React from 'react';
 
 import './menu-item.styles.scss'
 
-const MenuItem = ({title,size}) => (
-    <div className = {`${size} menu-item`}>
+const MenuItem = ({title,imgUrl,size}) => (
+    <div 
+        style = {{
+            backgroundImage : `url(${imgUrl})`
+        }}
+        className = {`${size} menu-item`}
+    >
                 <div className = 'content'>
                     <h1 className = 'title'> {title}  </h1>
                     <span className = 'subtitle'>Shop Now </span>
