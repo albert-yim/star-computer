558e56f59aa6a2b4b8ac29df5a3bcedb2e2417ca
chanyang719 <chanyang0719@gmail.com>
2020-11-30 14:40:03 -0800

    add muen-item, and finished styling menu-item.

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -3,17 +3,19 @@ import React from 'react';
 import './menu-item.styles.scss'
 
 const MenuItem = ({title,imgUrl,size}) => (
-    <div 
-        style = {{
-            backgroundImage : `url(${imgUrl})`
-        }}
-        className = {`${size} menu-item`}
-    >
-                <div className = 'content'>
-                    <h1 className = 'title'> {title}  </h1>
-                    <span className = 'subtitle'>Shop Now </span>
-                </div>
-            </div>
+    <div  className = {`${size} menu-item`}>
+        <div 
+            className = 'background-image'
+            style = {{
+                backgroundImage : `url(${imgUrl})`
+            }}
+        />
+
+        <div className = 'content'>
+            <h1 className = 'title'> {title}  </h1>
+            <span className = 'subtitle'>Shop Now </span>
+        </div>
+    </div>
 );
 
 export default MenuItem;
