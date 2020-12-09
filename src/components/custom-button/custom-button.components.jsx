4a3ea45f0dd9b95d451828e94eeffbfb990b1a81
chanyang719 <chanyang0719@gmail.com>
2020-12-09 14:17:18 -0800

    add feature that adding item to cart

--- src/components/custom-button/custom-button.components.jsx
+++ src/components/custom-button/custom-button.components.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 
 import './custom-button.styles.scss'
 
-const CustomButton = ({children, isGoogleSignIn, ...otehrProps}) => (
+const CustomButton = ({children, inverted, isGoogleSignIn, ...otehrProps}) => (
 
-    <button className = {`${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`} {...otehrProps}>
+    <button className = {`${inverted ? 'inverted' : ''}
+                            ${isGoogleSignIn ? 'google-sign-in' : ''} 
+                            custom-button`} {...otehrProps}>
         {children}
     </button>
 
