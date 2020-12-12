6d5cd74b61b75470dd52382f8f5e910361076f3f
chanyang719 <chanyang0719@gmail.com>
2020-12-11 16:10:05 -0800

    fix bug that item count was not update in the cart-icon

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -5,15 +5,25 @@ import { toggleCartHiiden } from '../../redux/cart/cart.actions';
 
 import './cart-icon.styles.scss';
 
-const CartIcon = ({toggleCartHiiden}) => (
-    <div className = 'cart-icon' onClick = {toggleCartHiiden}>
-        <ShoppingIcon className = 'shopping-icon'/>
-        <span className = 'item-count'> 0 </span>
+const CartIcon = ({toggleCartHiiden,itemCount}) => (
+<div className = 'cart-icon' onClick = {toggleCartHiiden}>
+<ShoppingIcon className = 'shopping-icon'/>
+        <span className = 'item-count'> {itemCount} </span>
     </div>
 );
+
+const mapStateToProps = state => ({
+    itemCount : state.cart.cartItems.reduce( 
+        (accum,item) => accum + item.quantity, 
+        0
+    )
+});
 
 const mapDispatchToProps = dispatch => ({
     toggleCartHiiden: () => dispatch(toggleCartHiiden())
 });
 
-export default connect(null,mapDispatchToProps)(CartIcon);
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+    )(CartIcon);
